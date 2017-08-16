1bfc409c6509d4b0f2a47983cc3c288e41b9e8c2
shadiqur <shadiqur@netg5.com>
2017-08-16 00:41:34 -0700

    demo of route

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
+import Vueroute from 'vue-router';
 import App from './App.vue'
+import product from './Product.vue'
+ Vue.use(Vueroute);
 
+const routes=[
+  {path:'/pro',component:product},
+  {path:'/',component:App},
+];
+const router=new Vueroute({
+routes,
+})
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+ // render: h => h(App),
+
 })
